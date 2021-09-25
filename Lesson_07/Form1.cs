cb32acc4b72b27a1b350a37120981ea7b18ca8ca
Dmitriy Tihomirov <tihomirovdima028@gmail.com>
2021-09-25 15:48:37 +0300

    Домашнее задание к уроку 7 Тихомирова Дмитрия

---
+++ Lesson_07/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_07
+{
+    public partial class Form1 : Form
+    {
+
+        int attempts = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public object inputNumberTextBot { get; private set; }
+
+        private Random random = new Random();
+        private int randomNumber = 0;
+        private void Reset()
+        {
+            randomNumber = random.Next(0, 100);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            if (int.TryParse(textBox1.Text, out int number))
+            {
+                if (number > randomNumber)
+                {
+                    MessageBox.Show($"Ваше число {number} больше загаданного",
+                        "Число не угадано", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    attempts++;
+                }
+                else if (number < randomNumber)
+                {
+                    MessageBox.Show($"Ваше число {number} меньше загаданного",
+                        "Число не угадано", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    attempts++;
+                }
+                else if (number == randomNumber)
+                {
+                    MessageBox.Show($"Ура! Победа! ВЫ угадали число. Ответ был: {number}. Ваше кол-во попыток:{attempts} ",
+                         "Число угадано", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+
+    private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+
---
+++ lesson_007/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson_007
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private int randomNumber = 0;
+        int attempt = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Reset()
+        {
+            randomNumber = random.Next(0, 100);
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberTextBox.Text, out int number))
+                {
+                if (randomNumber == number)
+                {
+                    MessageBox.Show($"Ура!Вы выиграли! Ответ был:{number}.Количество попыток:{attempt}",
+                        "Число уагаданно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    attempt++;
+                }
+                else if(randomNumber <= number){
+                    MessageBox.Show($"Ваше число {number} больше загаданного",
+                        "Число не уагаданно", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    attempt++;
+                }
+                else if (randomNumber >= number)
+                {
+                    MessageBox.Show($"Ваше число {number} меньше загаданного",
+                        "Число не уагаданно", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    attempt++;
+                }
+            }
+            else {
+                MessageBox.Show("Вы ввели некорректные данные");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Reset();
+            attempt = 0;
+        }
+    }
+}
